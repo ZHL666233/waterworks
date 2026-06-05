c641d8800212701ab1a854f8f65945b214b7f9e9
无敌暴龙战神 <3222407954@qq.com>
2026-06-05 14:11:46 +0800

    修复bat编码问题

--- 启动游戏.bat
+++ 启动游戏.bat
@@ -1,7 +1,8 @@
 @echo off
-set ROOT=%~dp0
-set ROOT=%ROOT:~0,-1%
-cd /d "%ROOT%"
-start "" "http://127.0.0.1:8777"
-powershell -ExecutionPolicy Bypass -File "%ROOT%\启动游戏.ps1"
+setlocal
+set "d=%~dp0"
+set "d=%d:~0,-1%"
+cd /d "%d%"
+start http://127.0.0.1:8777
+powershell -ExecutionPolicy Bypass -File "%d%\启动游戏.ps1"
 pause
